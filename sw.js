adffef188f0c60532a1619f88a85555ee607359b
HUSHPAPAYE <155306382+HUSHPAPAYE@users.noreply.github.com>
2025-09-03 22:41:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-const CACHE='shift90-v3';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE = "shift90pro-v1";
+const ASSETS = [
+  "./","./index.html","./style.css?v=1","./app.js?v=1",
+  "./manifest.webmanifest?v=1","./icon-512.png"
+];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
